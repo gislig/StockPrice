f94eb6330c50fbfb456c9e49c962f15cd132b6bf
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-21 22:21:18 +0000

    Final - Draft

--- stockPrice.py
+++ stockPrice.py
@@ -1,40 +1,32 @@
-# Enter number of shares: 100
-# Enter price (dollars, numerator, denominator): 29 a b
-# Invalid price!
-# Enter price (dollars, numerator, denominator): 29 7 8
-# 100 shares with market price 29 7/8 have value $2987.50
-# Continue: y
-# Enter number of shares: 1a
-# Invalid number!
-# Enter number of shares: 30
-# Enter price (dollars, numerator, denominator): x 1 2
-# Invalid price!
-# Enter price (dollars, numerator, denominator): 89 1 2
-# 30 shares with market price 89 1/2 have value $2685.00
-# Continue: n
+from fractions import Fraction
 
-def Stock_Price():
+def Get_Total_Dollars():
     while True:
+        try:
+            share_price_int = input("Enter price (dollars, numerator, denominator): ")
+            value_array = share_price_int.split(" ")
+            dollars = int(value_array[0])
+            numerator = int(value_array[1])
+            denominator = int(value_array[2])
+            total = float(Fraction(numerator/denominator))
+            return dollars, numerator, denominator, total
+        except Exception:
+            print("Invalid price!")
+
+def Get_Shares():
+    try:
         number_of_shares_int = int(input("Enter number of shares: "))
+        return number_of_shares_int
+    except Exception:
+        print("Invalid number!")
 
-        while True:
-            try:
-                share_price_int = input("Enter price (dollars, numerator, denominator): ")
-                value_array = share_price_int.split(" ")
-                dollars = int(value_array[0])
-                numerator = int(value_array[1])
-                denominator = int(value_array[2])
-                break
-            except ValueError:
-                print("Invalid price!")
-        #100 shares with market price 29 7/8 have value $2987.50
-        calc = number_of_shares_int * dollars * float(numerator/denominator)
-        print("{} shares with market price {} {}/{} have the value {}".format(number_of_shares_int, dollars,numerator,denominator,calc))
-        cont_str = input("Continue: ")
-        if cont_str in 'yY':
-            continue
-        elif cont_str in 'nN':
-            break
-
-
-Stock_Price()
+while True:
+    number_of_shares_int = Get_Shares()
+    dollars, numerator, denominator, total = Get_Total_Dollars()
+    calc = number_of_shares_int * (dollars + total)
+    print("{} shares with market price {} {}/{} have the value {:.2f}".format(number_of_shares_int, dollars,numerator,denominator,calc))
+    cont_str = input("Continue: ")
+    if cont_str in 'yY':
+        continue
+    elif cont_str in 'nN':
+        break
